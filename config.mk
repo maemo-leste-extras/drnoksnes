c32539d5892537b59907b2bb82176599476a19ed
Arno1103 <happarno@hotmail.fr>
2024-07-07 13:23:04 +0200

    first attempt to build without additional deps

--- config.mk
+++ config.mk
@@ -1,6 +1,7 @@
 ARCH=armhf
 CONF_GUI=1
 CONF_HD=0
+CONF_XSP=0
 ASFLAGS=-g
 CFLAGS=-Wall -g -DMAEMO -DMAEMO_VERSION=5 -O2 -fomit-frame-pointer -ffast-math
 CXXFLAGS=-Wall -g -DMAEMO -DMAEMO_VERSION=5 -O2 -fomit-frame-pointer -ffast-math -fno-exceptions -fno-rtti
